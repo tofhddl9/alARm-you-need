6337791c2781a01d2d3789c183fd6a92331a9b7e
tofhddl9 <tofhddl9@gmail.com>
2020-07-08 02:25:05 +0900

    Implement back_btn

--- app/src/main/java/com/example/alarm/AlarmSettingActivity.kt
+++ app/src/main/java/com/example/alarm/AlarmSettingActivity.kt
@@ -2,10 +2,23 @@ package com.example.alarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_alarm_setting.*
 
 class AlarmSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm_setting)
+
+        back_btn.setOnClickListener {
+            onBackPressed()
+        }
+
+        cancel_btn.setOnClickListener {
+
+        }
+
+        save_btn.setOnClickListener {
+
+        }
     }
 }
