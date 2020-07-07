a8e62c5dc266fdb4d2c8c979b70413b65177f827
tofhddl9 <tofhddl9@gmail.com>
2020-07-07 12:52:19 +0900

    Make adapter and viewholder

---
+++ app/src/main/java/com/example/alarm/AlarmDataAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.alarm
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.alarm_item.view.*
+
+class AlarmDataAdapter(val list : List<AlarmModel>): RecyclerView.Adapter<AlarmDataViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlarmDataViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.alarm_item, parent, false)
+        return AlarmDataViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.count()
+    }
+
+    override fun onBindViewHolder(holder: AlarmDataViewHolder, position: Int) {
+        holder.containerView.alarm_title.text = list[position].title
+        holder.containerView.alarm_apm.text = list[position].apm
+        holder.containerView.alarm_time.text = list[position].time
+        holder.containerView.alarm_day.text = list[position].day
+        holder.containerView.alarm_on_off.text = list[position].onoff
+    }
+
+}
