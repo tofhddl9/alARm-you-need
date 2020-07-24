a030926dfe612967a5f0662e28df5dec8b77ca2c
tofhddl9 <tofhddl9@gmail.com>
2020-07-24 17:02:25 +0900

    Seperate day:ByteArray into individual Boolean variable

--- app/src/main/java/com/example/alarm/AlarmData.kt
+++ app/src/main/java/com/example/alarm/AlarmData.kt
@@ -7,12 +7,15 @@ import java.util.*
 
 open class AlarmData(
     @PrimaryKey
-    var alarmId : Int = 0,
+    var alarmId: String =  UUID.randomUUID().toString(),
     var title:String = "",
     var hour:Int = 12,
+    var alarmTime:Date= Date(),
     var minute:Int = 34,
     var apm:String = "",
-    var day:ByteArray = ByteArray(7),
+    var sun:Boolean = false, var mon:Boolean = false, var tue:Boolean = false,
+    var wed:Boolean = false, var thur:Boolean = false,
+    var fri:Boolean = false, var sat:Boolean = false,
     var onoff:Boolean = true,
     var uriRingtone: String = "",
     var volume : Int = 5) : RealmObject(), Serializable {
