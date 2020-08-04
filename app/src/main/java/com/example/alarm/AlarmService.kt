d713f945d5cf614e3276d206cbc681911839e77b
tofhddl9 <tofhddl9@gmail.com>
2020-08-04 16:44:38 +0900

    Implement alarmService stop

--- app/src/main/java/com/example/alarm/AlarmService.kt
+++ app/src/main/java/com/example/alarm/AlarmService.kt
@@ -3,7 +3,6 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
-import android.media.RingtoneManager
 import android.net.Uri
 import android.os.IBinder
 import android.os.Vibrator
@@ -18,38 +17,36 @@ class AlarmService : Service() {
         super.onCreate()
         vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         Log.d("AlarmService::","onCreate() is called")
-
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        Log.d("AlarmService::","onStartCommand() is called")
+        Log.d("AlarmService::", "onStartCommand() is called")
 
         val alarmId = intent.getStringExtra("ALARM_ID")
         val realm = Realm.getDefaultInstance()
         val alarmData = AlarmDao(realm).selectAlarm(alarmId!!)
 
-        /*For debug*/
-        val uri = Uri.parse(alarmData.uriRingtone)
-        val ringtone = RingtoneManager.getRingtone(this, uri)
-        Log.d("ringtone : ", ringtone.getTitle(this))
-        Log.d("volume : ", ""+alarmData.volume)
-
         mediaPlayer = MediaPlayer.create(this, Uri.parse(alarmData.uriRingtone))
-        mediaPlayer.setVolume(1.0F*alarmData.volume, 1.0F*alarmData.volume)
+        mediaPlayer.setVolume(1.0F * alarmData.volume, 1.0F * alarmData.volume)
         mediaPlayer.isLooping = true
 
+        /*
+        * Todo : Make Vibration
+        *  */
+        vibrator.vibrate(3000)
         mediaPlayer.start()
-        return START_STICKY
 
-        /*운동 다녀와서
-        todo
-         volume 자료형 flaot으로 바꾸고 realm 충돌 확인
-         요일 설정 강제할지
-         AR 구현 시작해야겠다.
-         */
+        return START_STICKY
     }
 
     override fun onBind(intent: Intent): IBinder? {
         return null
+    }
+
+    override fun onDestroy() {
+        Log.d("AlarmService::", "onDestroy() is called")
+        super.onDestroy()
+        mediaPlayer.stop()
+        vibrator.cancel()
     }
 }
--- app/src/main/java/com/example/alarm/RingActivity.kt
+++ app/src/main/java/com/example/alarm/RingActivity.kt
@@ -1,25 +1,70 @@
 package com.example.alarm
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_ring.*
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.ArCoreApk
+import kotlinx.android.synthetic.main.activity_ring.*;
+import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper;
 
 class RingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("RingActivity::onCreate", "is called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ring)
 
         val alarmData = intent.getStringExtra("ALARM_ID")
+        val serviceIntent = Intent(this, AlarmService::class.java)
+        serviceIntent.putExtra("ALARM_ID", alarmData)
+        startService(serviceIntent)
 
-        val intent = Intent(this, AlarmService::class.java)
-        intent.putExtra("ALARM_ID", alarmData)
-        startService(intent)
-        Log.d("RingActivity::onCreate", "is called")
-
-        alarm_off.setOnClickListener { view ->
+        alarm_off.setOnClickListener {
+            val ringOffIntent = Intent(applicationContext, AlarmService::class.java)
+            applicationContext.stopService(ringOffIntent)
             finish()
+        }
+
+        maybeEnableArButton()
+    }
+/*
+    override fun onRequestPermissionsResult(requestCode: Int,  permissions: Array<out String>,  grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions!!, grantResults!!)
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(
+                this,
+                "Camera permission is needed to run this application",
+                Toast.LENGTH_LONG
+            )
+                .show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+            finish()
+        }
+    }
+*/
+    private fun maybeEnableArButton() {
+        val availability = ArCoreApk.getInstance().checkAvailability(this);
+        if (availability.isTransient) {
+            // Re-query at 5Hz while compatibility is checked in the background.
+            Handler().postDelayed({
+                run {
+                    maybeEnableArButton();
+                }
+            }, 200);
+        }
+        if (availability.isSupported) {
+            alarm_off.visibility = View.VISIBLE;
+            alarm_off.isEnabled = true;
+            // indicator on the button.
+        } else { // Unsupported or unknown.
+            alarm_off.visibility = View.INVISIBLE;
+            alarm_off.isEnabled = false;
         }
     }
 }
