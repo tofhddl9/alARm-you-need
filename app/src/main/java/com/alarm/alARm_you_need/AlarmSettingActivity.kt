437c21bfe601d4f0e1e5e59a592b45d064be8828
tofhddl9 <tofhddl9@gmail.com>
2020-09-02 11:31:24 +0900

    Implement image select dialog

--- app/src/main/java/com/alarm/alARm_you_need/AlarmSettingActivity.kt
+++ app/src/main/java/com/alarm/alARm_you_need/AlarmSettingActivity.kt
@@ -1,23 +1,19 @@
 package com.alarm.alARm_you_need
 import android.app.Activity
 import android.app.AlertDialog
-import android.app.Dialog
-import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.media.RingtoneManager
 import android.net.Uri
-import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import android.view.ContextThemeWrapper
-import android.view.LayoutInflater
 import android.view.View
-import android.view.WindowManager
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper
 import kotlinx.android.synthetic.main.activity_alarm_setting.*
 
@@ -25,9 +21,13 @@ class AlarmSettingActivity : AppCompatActivity() {
 
     private var viewModel: AlarmSettingViewModel? = null
     private lateinit var uriRingtone: String
+    private lateinit var uriArImage: String
     private lateinit var alarmType: String
 
-    private val RINGTONE_SELECT = 5;
+    private val REQ_RINGTONE_SELECT = 1
+    private val REQ_CAMERA_OPEN = 2
+    private val REQ_GALLERY_OPEN = 3
+    /* 요청 코드를 dialog와 중복으로 갖고 있는데 어떻게 해결할까 */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +37,7 @@ class AlarmSettingActivity : AppCompatActivity() {
         val defaultRingtone = RingtoneManager.getRingtone(this, defaultUri)
         ringtone_btn.text = defaultRingtone.getTitle(this)
         alarm_type_btn.text = AlarmData.TYPE_DEFAULT
+
 
         viewModel = application!!.let {
             ViewModelProvider(viewModelStore, ViewModelProvider.AndroidViewModelFactory(it))
@@ -57,8 +58,8 @@ class AlarmSettingActivity : AppCompatActivity() {
             it.sat.observe(this, Observer { sat_box.isChecked = it })
 
             it.uriRingtone.observe(this, Observer { uriRingtone = it })
-            it.volume.observe(this, Observer {volume_bar.progress = it})
-            it.alarmType.observe(this, Observer {alarmType = it})
+            it.volume.observe(this, Observer { volume_bar.progress = it })
+            it.alarmType.observe(this, Observer { alarmType = it })
         }
 
         val alarmId = intent.getStringExtra("ALARM_ID")
@@ -70,6 +71,94 @@ class AlarmSettingActivity : AppCompatActivity() {
         }
 
         setOnClickListeners(alarmId)
+    }
+
+
+    private fun isAlarmValid(): Boolean {
+        val isValid: Boolean = sun_box.isChecked or mon_box.isChecked or tue_box.isChecked or
+                wed_box.isChecked or thur_box.isChecked or fri_box.isChecked or sat_box.isChecked
+        return isValid
+    }
+
+    private fun writeRingtoneTitle(uri: Uri) {
+        val ringtone = RingtoneManager.getRingtone(this, uri)
+        ringtone_btn.text = ringtone.getTitle(this)
+    }
+
+    private fun writeAlarmTypeTitle(alarmType: String?) {
+        alarm_type_btn.text = alarmType
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQ_RINGTONE_SELECT -> {
+                    val uri =
+                        data!!.getParcelableExtra<Uri>(RingtoneManager.EXTRA_RINGTONE_PICKED_URI)
+                    if (uri != null) {
+                        uriRingtone = uri.toString()
+                        writeRingtoneTitle(uri!!)
+                    }
+                }
+
+                REQ_GALLERY_OPEN -> {
+                    val imageUri = data?.data!!
+                    try {
+                        val input = contentResolver.openInputStream(imageUri)
+                        val image = BitmapFactory.decodeStream(input)
+                        Glide.with(this)
+                            .load(image)
+                            .into(ar_image)
+
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                }
+            }
+
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            //nop
+        }
+    }
+
+    private fun showConfirmAlert(alarmId: String) {
+        val builder = AlertDialog.Builder(
+            ContextThemeWrapper(
+                this@AlarmSettingActivity,
+                R.style.Theme_AppCompat_Light_Dialog
+            )
+        )
+        builder.setTitle("알람 삭제")
+        builder.setMessage("알람을 삭제하시겠습니까?")
+
+        builder.setPositiveButton("확인") { _, _ ->
+            Log.d("delete button", "is clicked")
+            viewModel?.deleteAlarm(alarmId)
+            finish()
+        }
+        builder.setNegativeButton("취소") { _, _ ->
+
+        }
+        builder.show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "AR 기능을 사용하기 위해서 카메라 권한이 필요합니다", Toast.LENGTH_LONG).show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                Log.d("DEBUGGING LOG", "HERE I AM")
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+        }
+        else {
+            alarmType = "AR"
+        }
     }
 
     private fun setOnClickListeners(alarmId: String?) {
@@ -118,23 +207,27 @@ class AlarmSettingActivity : AppCompatActivity() {
         alarm_type_btn.setOnClickListener {
             val alarmTypes = arrayOf("DEFAULT", "AR")
             val checkedItem = alarmTypes.indexOf(alarmType)
+            var type = "DEFAULT"
             builder.setSingleChoiceItems(alarmTypes, checkedItem) { _, which ->
-                alarmType = alarmTypes[which]
-                if (alarmType == "AR") {
+                type = alarmTypes[which]
+                if (type == "AR") {
                     if (!CameraPermissionHelper.hasCameraPermission(this)) {
-                        alarmType = "DEFAULT"
+                        type = "DEFAULT"
                         ar_image.visibility = View.GONE
                         CameraPermissionHelper.requestCameraPermission(this)
                     }
                     else {
+                        type = "AR"
                         ar_image.visibility = View.VISIBLE
                     }
                 }
                 else {
+                    type = "DEFAULT"
                     ar_image.visibility = View.GONE
                 }
             }
             builder.setPositiveButton("Ok") { _, _ ->
+                alarmType = type
                 alarm_type_btn.text = alarmType
             }
             builder.setNegativeButton("Cancel", null)
@@ -149,67 +242,8 @@ class AlarmSettingActivity : AppCompatActivity() {
 
     }
 
-    private fun isAlarmValid(): Boolean {
-        var isValid = false
-        isValid = isValid or sun_box.isChecked or mon_box.isChecked or tue_box.isChecked or
-                wed_box.isChecked or thur_box.isChecked or fri_box.isChecked or sat_box.isChecked
-        return isValid
-    }
 
-    private fun writeRingtoneTitle(uri: Uri) {
-        val ringtone = RingtoneManager.getRingtone(this, uri)
-        ringtone_btn.text = ringtone.getTitle(this)
-    }
 
-    private fun writeAlarmTypeTitle(alarmType: String?) {
-        alarm_type_btn.text = alarmType
-    }
 
-    override fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (resultCode == Activity.RESULT_OK && requestCode == RINGTONE_SELECT) {
-            val uri = data!!.getParcelableExtra<Uri>(RingtoneManager.EXTRA_RINGTONE_PICKED_URI)
-            if (uri != null) {
-                uriRingtone = uri.toString()
-                writeRingtoneTitle(uri!!)
-            }
-        }
-    }
-
-    private fun showConfirmAlert(alarmId: String) {
-        val builder = AlertDialog.Builder(
-            ContextThemeWrapper(
-                this@AlarmSettingActivity,
-                R.style.Theme_AppCompat_Light_Dialog
-            )
-        )
-        builder.setTitle("알람 삭제")
-        builder.setMessage("알람을 삭제하시겠습니까?")
-
-        builder.setPositiveButton("확인") { _, _ ->
-            Log.d("delete button", "is clicked")
-            viewModel?.deleteAlarm(alarmId)
-            finish()
-        }
-        builder.setNegativeButton("취소") { _, _ ->
-
-        }
-        builder.show()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            Toast.makeText(this, "AR 기능을 사용하기 위해서 카메라 권한이 필요합니다", Toast.LENGTH_LONG).show()
-            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-                Log.d("DEBUGGING LOG", "HERE I AM")
-                // Permission denied with checking "Do not ask again".
-                CameraPermissionHelper.launchPermissionSettings(this)
-            }
-        }
-        else {
-            alarmType = "AR"
-        }
-    }
 }
+
--- app/src/main/java/com/alarm/alARm_you_need/ImageDialog.kt
+++ app/src/main/java/com/alarm/alARm_you_need/ImageDialog.kt
@@ -1,12 +1,20 @@
 package com.alarm.alARm_you_need
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.Toast
 
 class ImageDialog constructor(context: Context) : Dialog(context) {
+
+    private val activity = context as Activity
+    private val REQ_CAMERA_OPEN = 2
+    private val REQ_GALLERY_OPEN = 3
+    private val REQ_PREVIEW_OPEN = 4
 
     init {
         val alertDialog = AlertDialog.Builder(context).create()
@@ -17,19 +25,22 @@ class ImageDialog constructor(context: Context) : Dialog(context) {
         alertDialog.window?.setLayout(700, WindowManager.LayoutParams.WRAP_CONTENT)
 
         val cameraButton = view.findViewById<Button>(R.id.camera_req)
-        val galeryButton = view.findViewById<Button>(R.id.galery_req)
-        val removeButton = view.findViewById<Button>(R.id.remove_img)
+        val galleryButton = view.findViewById<Button>(R.id.galery_req)
+        val previewButton = view.findViewById<Button>(R.id.preview_img)
 
         cameraButton.setOnClickListener {
-
+            Toast.makeText(context, "아직 미지원 기능입니다", Toast.LENGTH_SHORT).show()
+            /* 단순히 사진 찍기x. 맞춰서 자르는 기능 필요 */
         }
 
-        galeryButton.setOnClickListener {
-
+        galleryButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            activity.startActivityForResult(intent, REQ_GALLERY_OPEN)
         }
 
-        removeButton.setOnClickListener {
-
+        previewButton.setOnClickListener {
+            Toast.makeText(context, "아직 미지원 기능입니다", Toast.LENGTH_SHORT).show()
         }
 
     }
