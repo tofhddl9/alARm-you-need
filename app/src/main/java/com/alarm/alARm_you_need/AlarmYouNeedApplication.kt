3cb4cbe6a290b87e6b4df7549dc3a7bf1d88cd2c
tofhddl9 <tofhddl9@gmail.com>
2020-09-14 01:13:04 +0900

    Implement onboardingActivity except description

--- app/src/main/java/com/alarm/alARm_you_need/AlarmYouNeedApplication.kt
+++ app/src/main/java/com/alarm/alARm_you_need/AlarmYouNeedApplication.kt
@@ -9,15 +9,12 @@ class AlarmYouNeedApplication : Application() {
         super.onCreate()
         Realm.init(this)
         Log.d("DEBUGGING LOG", "AlarmYouNeedApplication")
-        Thread.sleep(1500)
-        setTheme(R.style.AppTheme)
     }
 
 }
 
 /* Todo List
 * - 활성화 알람 시간 순 정렬
-* - 다음 알람까지 남은 시간 계산
 * - ARCore, Sceneform 버전 확인해보자... 사용중인 버전 성능이 안 좋다
 *
 * */
--- app/src/main/java/com/alarm/alARm_you_need/OnboardingActivity.kt
+++ app/src/main/java/com/alarm/alARm_you_need/OnboardingActivity.kt
@@ -1,15 +1,28 @@
 package com.alarm.alARm_you_need
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_onboarding.*
 
 class OnboardingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (restorePreferenceData()) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         Thread.sleep(1500)
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
@@ -26,5 +39,61 @@ class OnboardingActivity : AppCompatActivity() {
         TabLayoutMediator(tab_indicator, screen_viewpager) {tab, position->
 
         }.attach()
+
+        btn_next.setOnClickListener {
+            var position = screen_viewpager.currentItem
+            if (position < list.size) {
+                position++
+                screen_viewpager.currentItem = position
+            }
+            if (position == list.size - 1) {
+                loadLastScreen()
+            }
+        }
+
+        tab_indicator.addOnTabSelectedListener (object : OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                if (tab.position == list.size -1) {
+                    loadLastScreen()
+                }
+            }
+        })
+
+        btn_get_started.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            savePreferenceData()
+        }
+    }
+
+    private fun restorePreferenceData(): Boolean {
+        val sharedPreference = applicationContext.getSharedPreferences("onboardingPref", Context.MODE_PRIVATE)
+
+        return sharedPreference.getBoolean("isOnboardingOpened", false)
+    }
+
+    private fun savePreferenceData() {
+        val sharedPreference = applicationContext.getSharedPreferences("onboardingPref", Context.MODE_PRIVATE)
+        val editor = sharedPreference.edit()
+        editor.putBoolean("isOnboardingOpened", true)
+        editor.apply()
+    }
+
+    private fun loadLastScreen() {
+        btn_next.visibility = View.INVISIBLE
+        tab_indicator.visibility = View.INVISIBLE
+        btn_get_started.visibility = View.VISIBLE
+
+        val btnAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.button_animation)
+        btn_get_started.animation = btnAnimation
     }
 }
