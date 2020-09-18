d8b8bc7ca69510d19ebef4e2a9a429ea56e7be56
tofhddl9 <tofhddl9@gmail.com>
2020-09-18 15:26:43 +0900

    Show splash image when application start

--- app/src/main/java/com/alarm/alARm_you_need/AlarmYouNeedApplication.kt
+++ app/src/main/java/com/alarm/alARm_you_need/AlarmYouNeedApplication.kt
@@ -9,12 +9,8 @@ class AlarmYouNeedApplication : Application() {
         super.onCreate()
         Realm.init(this)
         Log.d("DEBUGGING LOG", "AlarmYouNeedApplication")
+
+        Thread.sleep(1500)
+        setTheme(R.style.AppTheme)
     }
-
 }
-
-/* Todo List
-* - 활성화 알람 시간 순 정렬
-* - ARCore, Sceneform 버전 확인해보자... 사용중인 버전 성능이 안 좋다
-*
-* */
