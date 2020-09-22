075ece298d5e483df251172696bc499bdf07b97e
tofhddl9 <tofhddl9@gmail.com>
2020-09-22 22:46:54 +0900

    Add native ad in closeDialog

--- app/src/main/java/com/alarm/alARm_you_need/CloseAppDialog.kt
+++ app/src/main/java/com/alarm/alARm_you_need/CloseAppDialog.kt
@@ -1,13 +1,16 @@
 package com.alarm.alARm_you_need
 
+import android.content.Context
+import android.graphics.Point
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.DialogFragment
+import com.google.android.gms.ads.*
 import kotlinx.android.synthetic.main.close_app_dialog.*
 
 class CloseAppDialog: DialogFragment() {
+
+    private lateinit var size: Point
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +23,19 @@ class CloseAppDialog: DialogFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        MobileAds.initialize(requireContext()) { }
+        val adLoader = AdLoader.Builder(requireContext(), "ca-app-pub-3940256099942544/2247696110")
+            .forUnifiedNativeAd {
+                close_dialog_ad.setNativeAd(it)
+            }
+            .build()
+        adLoader.loadAd(AdRequest.Builder().build())
+
+        val windowManager = requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        size = Point()
+        display.getSize(size)
+
         close_app_btn.setOnClickListener {
             (activity as MainActivity).finish()
         }
@@ -28,4 +44,13 @@ class CloseAppDialog: DialogFragment() {
             dismiss()
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
+        val deviceWidth = size.x
+        params?.width = (deviceWidth * 0.9).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
+    }
+
 }
