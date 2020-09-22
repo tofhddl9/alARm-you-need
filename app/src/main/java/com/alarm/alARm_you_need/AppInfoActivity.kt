5f8a5b1db36f6c53e76c39a01f428730266219b3
tofhddl9 <tofhddl9@gmail.com>
2020-09-23 00:09:36 +0900

    Modify admob test id to real id

--- app/src/main/java/com/alarm/alARm_you_need/AppInfoActivity.kt
+++ app/src/main/java/com/alarm/alARm_you_need/AppInfoActivity.kt
@@ -1,6 +1,7 @@
 package com.alarm.alARm_you_need
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_app_info.*
 
@@ -14,11 +15,11 @@ class AppInfoActivity : AppCompatActivity() {
         app_version.text = appVersion
 
         btn_suggestion.setOnClickListener {
-
+            Toast.makeText(this, "업데이트 예정입니다.", Toast.LENGTH_SHORT).show()
         }
 
         btn_show_license.setOnClickListener {
-
+            Toast.makeText(this, "업데이트 예정입니다.", Toast.LENGTH_SHORT).show()
         }
     }
 }
