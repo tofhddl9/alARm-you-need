7950e7d51cd4159b029c364222c0f6aa5efb39ec
tofhddl9 <tofhddl9@gmail.com>
2020-10-17 02:06:48 +0900

    Call setNativeAd() only when close_dialog_ad is not null

--- app/src/main/java/com/alarm/alARm_you_need/CloseAppDialog.kt
+++ app/src/main/java/com/alarm/alARm_you_need/CloseAppDialog.kt
@@ -3,6 +3,7 @@ package com.alarm.alARm_you_need
 import android.content.Context
 import android.graphics.Point
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.DialogFragment
 import com.google.android.gms.ads.*
@@ -24,9 +25,10 @@ class CloseAppDialog: DialogFragment() {
         super.onActivityCreated(savedInstanceState)
 
         MobileAds.initialize(requireContext()) { }
+
         val adLoader = AdLoader.Builder(requireContext(), "ca-app-pub-3940256099942544/2247696110")
             .forUnifiedNativeAd {
-                close_dialog_ad.setNativeAd(it)
+                close_dialog_ad?.setNativeAd(it)
             }
             .build()
         adLoader.loadAd(AdRequest.Builder().build())
